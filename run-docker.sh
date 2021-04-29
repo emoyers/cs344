3a663bc5f928e3cd4f46969676408a2503f1a180
Emilio Moyers <emoyersb@gmail.com>
2021-04-29 21:25:18 +0000

    adding docker related changes to avoid entering the docker image in root user

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,7 @@
 xhost +
 docker run -it --rm --gpus all\
       --network=host \
-      -e "NUID=$(id -u)" -e "NGID=$(id -g)" \
+      --user "$(id -u):$(id -g)" \
       -e QT_X11_NO_MITSHM=1 -e DISPLAY=$DISPLAY -v /tmp:/tmp:rw --privileged \
-      -v /home/t8162em/Documents/Other/Cuda:/workspace cuda_11.2_opencv_4.5.2:latest
+      -v ~/.gitconfig:/etc/gitconfig \
+      -v /home/emilio/Documents/Cuda:/workspace cuda_11.2_opencv_4.5.2_user:latest bash
